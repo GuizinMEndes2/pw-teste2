8e497769c72f6ce066899a2da94b32f112042027
GuizinMEndes2 <126169397+GuizinMEndes2@users.noreply.github.com>
2023-06-01 09:38:58 -0300

    insert produtos

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -2,7 +2,22 @@
 @section('title', 'Produtos')
 
 @section('content')
-    <p>Produtos funcionando</p>
+    <table border="1" style="border-color:blue">
+        <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+        </tr>
+        @foreach ($prods as $prod )
+
+
+        <tr>
+            <td>{{ $prod->name }}</td>
+            <td>R$ {{ number_format($prod->price,2, ',', '.') }}</td>
+            <td>{{ $prod->quantity}}</td>
+        </tr>
+        @endforeach
+    </table>
     <a href="{{ route('produtos.add') }}">Adicionar produtos</a>
 @endsection
 
